eb65a932803664348cdcdd9e545fd93fd9fa7ad0
Arseniy <arseniy.karmanov@mail.ru>
2021-01-28 21:37:17 +0900

    comm - 7.4.3 heroku

--- src/main/java/com/karmanchik/chtotib_bot_rest_service/ChtotibBotRestServiceApplication.java
+++ src/main/java/com/karmanchik/chtotib_bot_rest_service/ChtotibBotRestServiceApplication.java
@@ -1,15 +1,13 @@
 package com.karmanchik.chtotib_bot_rest_service;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import javax.annotation.PostConstruct;
 import java.util.TimeZone;
 
 
-@ComponentScan
-@EnableAutoConfiguration
+@SpringBootApplication
 public class ChtotibBotRestServiceApplication {
 
     public static void main(String[] args) {
