796c9d12cbc7394a929561f5f0740e73e10786b2
Арсений Карманов <arseniy.karmanov@mail.ru>
2021-05-14 11:52:58 +0900

    Commit - 15

--- src/main/java/com/karmanchik/chtotib_bot_rest_service/configuration/MyConfiguration.java
+++ src/main/java/com/karmanchik/chtotib_bot_rest_service/configuration/MyConfiguration.java
@@ -1,7 +1,6 @@
 package com.karmanchik.chtotib_bot_rest_service.configuration;
 
 import lombok.extern.log4j.Log4j2;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
@@ -11,25 +10,22 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import javax.annotation.PostConstruct;
 import java.util.TimeZone;
 
-@Configuration
-@EnableAsync
-@EnableTransactionManagement
 @Log4j2
-public class MyConfiguration {
+@EnableAsync
+@Configuration
+@EnableTransactionManagement
+public class MyConfiguration implements WebMvcConfigurer {
 
-    @Bean
-    public WebMvcConfigurer webMvcConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/api/**")
-                        .allowedOrigins("*")
-                        .allowedMethods("PUT", "DELETE", "GET", "POST")
-                        .allowedHeaders("Accept", "Content-Type", "Host", "Age")
-                        .exposedHeaders("*")
-                        .allowCredentials(false).maxAge(3600);
-            }
-        };
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        log.info("Create WebMvcConfigurer");
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("PUT", "DELETE", "GET", "POST")
+                .allowedHeaders("Accept", "Content-Type", "Host", "Age")
+                .exposedHeaders("*")
+                .allowCredentials(false).maxAge(3600);
     }
 
     @PostConstruct
